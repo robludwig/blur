d25fe25823c60d3aa96749198652f5ce562b6952
robludwig <robludwig@gmail.com>
2014-07-05 18:19:15 -0500

    output to a file if specified on the command line

--- blur.py
+++ blur.py
@@ -4,7 +4,7 @@ import opencvfxns
 if __name__ == '__main__':
 	print "blur.py: simple face detector to blur faces in an image"
 	if len(sys.argv) < 2:
-		print "usage: blur.py image.png"
+		print "usage: blur.py image [output]"
 		sys.exit(0)
 	image = opencvfxns.load_image(sys.argv[1])
 	gray_image = opencvfxns.gray_image(image) #do the detection on a grayscale image to speed it up
@@ -12,4 +12,8 @@ if __name__ == '__main__':
 	for face in faces:
 		x, y, w, h = face
         opencvfxns.blur_region(image,x,y,w,h)
-	opencvfxns.save_image( image,sys.argv[1])
+	if sys.argv[2]:
+		output = sys.argv[2]
+	else:
+		output = sys.argv[1]
+	opencvfxns.save_image( output)
